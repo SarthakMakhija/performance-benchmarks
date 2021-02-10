9c4cc1a4caf94c54b300d7bd4723b8c80ac6b7de
sarthak.makhija <sarthak.makhija@gmail.com>
2021-02-10 13:57:42 +0530

    Sarthak | Updates Microbenchmarks

--- src/jmh/java/org/learning/tech/validator/OptimizedAlphanumericValidatorMicroBenchmark.java
+++ src/jmh/java/org/learning/tech/validator/OptimizedAlphanumericValidatorMicroBenchmark.java
@@ -1,48 +1,24 @@
 package org.learning.tech.validator;
 
 import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.infra.Blackhole;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@State(Scope.Benchmark)
 public class OptimizedAlphanumericValidatorMicroBenchmark {
 
-    private List<String> contentPassingAlphanumericValidation;
-    private List<String> contentFailingAlphanumericValidation;
-
-    @Param({"10000"})
-    private int totalContentItemsToBeValidated;
-
-    @Param({"1000000"})
-    private int totalBenchmarkIterations;
-
-    @Setup(Level.Trial)
-    public void setup() {
-        contentPassingAlphanumericValidation = new ArrayList<>();
-        contentFailingAlphanumericValidation = new ArrayList<>();
-        for (int count = 1; count <= totalContentItemsToBeValidated; count++) {
-            contentPassingAlphanumericValidation.add("contentPassingAlphanumericValidation" + count);
-            contentFailingAlphanumericValidation.add("content-Failing-Alphanumeric-Validation" + count);
-        }
+    @org.openjdk.jmh.annotations.State(Scope.Thread)
+    public static class State {
+        public String contentPassingAlphanumericValidation = "contentPassingAlphanumericValidation";
+        public String contentFailingAlphanumericValidation = "content-Failing-Alphanumeric-Validation";
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
-    public void isValid(Blackhole blackhole) {
-        for (int count = 1; count <= totalBenchmarkIterations; count++) {
-            boolean isValid = OptimizedAlphanumericValidator.instance.isValid(contentPassingAlphanumericValidation.get(count % totalContentItemsToBeValidated));
-            blackhole.consume(isValid);
-        }
+    public boolean isValid(State state) {
+        return OptimizedAlphanumericValidator.instance.isValid(state.contentPassingAlphanumericValidation);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
-    public void isNotValid(Blackhole blackhole) {
-        for (int count = 1; count <= totalBenchmarkIterations; count++) {
-            boolean isValid = OptimizedAlphanumericValidator.instance.isValid(contentFailingAlphanumericValidation.get(count % totalContentItemsToBeValidated));
-            blackhole.consume(isValid);
-        }
+    public boolean isNotValid(State state) {
+        return OptimizedAlphanumericValidator.instance.isValid(state.contentFailingAlphanumericValidation);
     }
 }
